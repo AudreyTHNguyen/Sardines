630b31187b5577fbce2a9d1b0feec3639131f936
Wingzfan98 <47340516+Wingzfan98@users.noreply.github.com>
2020-03-30 15:28:43 -0400

    Added the contact form

--- Nurse Capstone 3-24/n4.php
+++ Nurse Capstone 3-24/n4.php
@@ -98,6 +98,24 @@
                 </div>
             </div>
             <br>
+          <form action="mail_handler.php" method="post">
+                    <fieldset>
+                        <div><label for="first_name">First Name</label></div>
+                        <input type="text" name="first_name" placeholder="Your name..">
+
+                        <div><label for="last_name">Last Name</label></div>
+                        <input type="text" name="last_name" placeholder="Your last name..">
+
+                        <div><label for="email">Email</label></div>
+                        <input type="text" name="email" placeholder="Your email..">
+                        <div><label for="message">Questions? </label></div>
+                        <textarea name="message" cols="40" rows="5" placeholder="Your text.."></textarea>
+
+
+                        <div><input type="submit" name="submit" value="Submit"></div>
+                    </fieldset>
+                </form>
+
         </div>
     </div>
 
