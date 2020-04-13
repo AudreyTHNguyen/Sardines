8e4ec0b7806a463045b316cfaa847ad6a42086a9
TheHandman <35011645+TheHandman@users.noreply.github.com>
2020-04-12 23:21:16 -0400

    Reverted index.php

--- Nurse Capstone 3-24/index.php
+++ Nurse Capstone 3-24/index.php
@@ -1,10 +1,17 @@
-<?php
+<?php 
+  session_start(); 
 
-    header("Location: Home.php");
+  if (!isset($_SESSION['username'])) {
+  	$_SESSION['msg'] = "You must log in first";
+  	header('location: login.php');
+  }
+  if (isset($_GET['logout'])) {
+  	session_destroy();
+  	unset($_SESSION['username']);
+  	header("location: login.php");
+  }
+?>
 
-    exit();
-
-    ?>
 <!DOCTYPE html>
 <html>
 
